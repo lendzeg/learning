d6c03bf0a2da99262d2ba9ba5f8946410d8037ce
Nelson Rodriguez <lendzeg.public@gmail.com>
2025-09-01 22:18:59 -0500

    Dart learning project created

---
+++ dart/basics/app_basics/bin/app_basics.dart
@@ -0,0 +1,5 @@
+import 'package:app_basics/app_basics.dart' as app_basics;
+
+void main(List<String> arguments) {
+  print("Hola");
+}
---
+++ dart/basics/app_basics/test/app_basics_test.dart
@@ -0,0 +1,8 @@
+import 'package:app_basics/app_basics.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
